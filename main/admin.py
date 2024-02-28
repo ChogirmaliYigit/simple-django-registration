bca3cf32c35e67a247387ac099080ddddb37c50f
ChogirmaliYigit <chogirmali.yigit@gmail.com>
2024-02-28 22:00:00 +0500

    added subscription model

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from unfold.admin import ModelAdmin
-from main.models import Person
+from main.models import Person, SubscriptionPlan, SubscriptionPlanUser
 
 
 @admin.register(Person)
@@ -18,4 +18,30 @@ class PersonAdmin(ModelAdmin):
         "kadastr_image",
     )
     search_fields = fields + ("id",)
+    list_filter_submit = True
+
+
+@admin.register(SubscriptionPlan)
+class SubscriptionPlanAdmin(ModelAdmin):
+    list_display = ("title", "description", "price",)
+    fields = (
+        "title",
+        "description",
+        "price",
+    )
+    search_fields = fields + ("id",)
+    list_filter = ("price",)
+    list_filter_submit = True
+
+
+@admin.register(SubscriptionPlanUser)
+class SubscriptionPlanUserAdmin(ModelAdmin):
+    list_display = ("user", "plan", "is_active",)
+    fields = (
+        "user",
+        "plan",
+        "is_active",
+    )
+    search_fields = fields + ("id",)
+    list_filter = ("is_active",)
     list_filter_submit = True
--- main/models.py
+++ main/models.py
@@ -20,3 +20,32 @@ class Person(models.Model):
         db_table = "people"
         verbose_name = "Ro'yxatga yozilgan shaxs"
         verbose_name_plural = "Ro'yxatga yozilgan shaxslar"
+
+
+class SubscriptionPlan(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    price = models.DecimalField(max_digits=20, decimal_places=2)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        db_table = "subscription_plans"
+        verbose_name = "Ta'rif"
+        verbose_name_plural = "Ta'riflar"
+
+
+class SubscriptionPlanUser(models.Model):
+    user = models.ForeignKey(Person, on_delete=models.PROTECT)
+    plan = models.ForeignKey(SubscriptionPlan, on_delete=models.PROTECT)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.plan.title} - {self.user.full_name}"
+
+    class Meta:
+        db_table = "subscription_plan_users"
+        unique_together = ("user", "plan",)
+        verbose_name = "Ta'rifga obuna bo'lgan shaxs"
+        verbose_name_plural = "Ta'rifga obuna bo'lgan shaxslar"
